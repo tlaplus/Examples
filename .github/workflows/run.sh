3f2f0d304d9d2fa5a0b520b3a866ea0bcd5a79f9
Andrew Helwer <2n8rn1w1f@mozmail.com>
2023-01-28 11:34:09 -0500

    Added simulation specs
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -6,17 +6,7 @@ TLC_COMMAND="java -Dtlc2.TLC.stopAfter=180 -Dtlc2.TLC.ide=Github -Dutil.Executio
 ## https://github.com/tlaplus/Examples/pull/31#issuecomment-796354291
 #echo Check MCInnerSerial
 #$TLC_COMMAND  specifications/SpecifyingSystems/AdvancedExamples/MCInnerSerial
-echo Check EWD840_anim
-$TLC_COMMAND -simulate num=1 specifications/ewd840/EWD840_anim || (($? == 12))  ## Expect a safety violation
 echo Check EWD840_json
 sed -i '/^SendMsg/{n;d;}' specifications/ewd840/EWD840.tla ## Cause RealInv to be violated (see EWD840_json.tla)
 $TLC_COMMAND specifications/ewd840/EWD840_json
-echo Check EWD998ChanID
-$TLC_COMMAND specifications/ewd998/EWD998ChanID
-echo Simulate EWD687a_anim
-$TLC_COMMAND -simulate num=100 -note specifications/ewd687a/EWD687a_anim || (($? == 12))  ## Expect a safety violation
-echo Simulate KnuthYao
-$TLC_COMMAND -generate specifications/KnuthYao/SimKnuthYao
-echo Check Dijkstra Mutext
-$TLC_COMMAND specifications/dijkstra-mutex/DijkstraMutex.toolbox/LSpec-model/MC
 
