3787e528a525c3f386c3a8513d959cdbdaf1d2d8
Andrew Helwer <2n8rn1w1f@mozmail.com>
2023-01-27 19:38:20 -0500

    Categorized almost all models
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- .github/scripts/check_small_models.py
+++ .github/scripts/check_small_models.py
@@ -4,9 +4,48 @@ models should finish executing in less than ten seconds on the GitHub CI
 machines.
 """
 
+from concurrent.futures import ThreadPoolExecutor
 import json
+import logging
 from os import cpu_count
 import subprocess
+from timeit import default_timer as timer
+
+tlc_result = {
+    0   : 'success',
+    1   : 'stuttering failure',
+    11  : 'deadlock failure',
+    12  : 'safety failure',
+    13  : 'liveness failure'
+}
+
+def check_model(model_tuple):
+    start_time = timer()
+    module_path, model_path, expected_result = model_tuple
+    tlc = subprocess.run([
+        'java',
+        '-Dtlc2.TLC.stopAfter=60',
+        '-Dtlc2.TLC.ide=Github',
+        '-Dutil.ExecutionStatisticsCollector.id=abcdef60f238424fa70d124d0c77ffff',
+        '-XX:+UseParallelGC',
+        '-cp', 'tla2tools.jar',
+        'tlc2.TLC',
+        '-workers', '1',
+        '-lncheck', 'final',
+        '-tool',
+        '-config', model_path,
+        module_path
+    ], capture_output=True)
+    end_time = timer()
+    logging.info(f'{model_path} in {end_time - start_time:.1f}s')
+    actual_result = tlc_result[tlc.returncode] if tlc.returncode in tlc_result else str(tlc.returncode)
+    if expected_result != actual_result or '687' in model_path:
+        logging.error(f'Model {model_path} expected result {expected_result} but got {actual_result}')
+        logging.error(tlc.stdout.decode('utf-8'))
+        return False
+    return True
+
+logging.basicConfig(level=logging.INFO)
 
 manifest = None
 with open('manifest.json', 'rt') as manifest_file:
@@ -19,26 +58,12 @@ small_models = [
     for model in module['models'] if model['size'] == 'small'
 ]
 
-success = True
-for module_path, model_path, expected_result in small_models:
-    print(model_path)
-    tlc = subprocess.run([
-        'java',
-        '-Dtlc2.TLC.ide=Github',
-        '-Dutil.ExecutionStatisticsCollector.id=abcdef60f238424fa70d124d0c77ffff',
-        '-XX:+UseParallelGC',
-        '-cp', 'tla2tools.jar',
-        'tlc2.TLC',
-        '-workers', str(cpu_count()),
-        '-lncheck', 'final',
-        '-tool',
-        '-config', model_path,
-        module_path
-    ], capture_output=True)
-    if (tlc.returncode == 0 and expected_result != 'success') or (tlc.returncode != 0 and expected_result == 'success'):
-        success = False
-        print(f'Model {model_path} expected result {expected_result} but returned error code {tlc.returncode}')
-        print(tlc.stdout.decode('utf-8'))
-
-exit(0 if success else 1)
+# Since these models are small we parallelize by checking multiple files at
+# once instead of parallelizing state computation and exploration for each
+# individual model.
+thread_count = cpu_count()
+logging.info(f'Checking models using {thread_count} threads')
+with ThreadPoolExecutor(thread_count) as executor:
+    results = executor.map(check_model, small_models)
+    exit(0 if all(results) else 1)
 
