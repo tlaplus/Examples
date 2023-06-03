a999881b9b5886e23159ee0a0f4614651570196a
Andrew Helwer <2n8rn1w1f@mozmail.com>
2023-06-03 15:25:20 -0400

    Parameterized model checking scripts
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- .github/scripts/check_small_models.py
+++ .github/scripts/check_small_models.py
@@ -4,7 +4,9 @@ models should finish executing in less than ten seconds on the GitHub CI
 machines.
 """
 
+from argparse import ArgumentParser
 import logging
+from os.path import dirname, join, normpath
 from timeit import default_timer as timer
 import tla_utils
 
@@ -15,14 +17,33 @@ tlc_result = {
     13  : 'liveness failure'
 }
 
+parser = ArgumentParser(description='Checks all small TLA+ models in the tlaplus/examples repo using TLC.')
+parser.add_argument('--tools_jar_path', help='Path to the tla2tools.jar file', required=True)
+parser.add_argument('--tlapm_lib_path', help='Path to the TLA+ proof manager module directory; .tla files should be in this directory', required=True)
+parser.add_argument('--community_modules_jar_path', help='Path to the CommunityModules-deps.jar file', required=True)
+parser.add_argument('--manifest_path', help='Path to the tlaplus/examples manifest.json file', required=True)
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.INFO)
+
+tools_jar_path = normpath(args.tools_jar_path)
+tlapm_lib_path = normpath(args.tlapm_lib_path)
+community_jar_path = normpath(args.community_modules_jar_path)
+manifest_path = normpath(args.manifest_path)
+examples_root_path = dirname(manifest_path)
+
 def check_model(module_path, model, expected_runtime):
-    model_path = model['path']
+    module_path = normpath(join(examples_root_path, module_path))
+    model_path = normpath(join(examples_root_path, model['path']))
     logging.info(model_path)
     expected_result = model['result']
     start_time = timer()
     tlc, hit_timeout = tla_utils.check_model(
+        tools_jar_path,
         module_path,
         model_path,
+        tlapm_lib_path,
+        community_jar_path,
         model['mode'],
         model['config'],
         60
@@ -39,11 +60,8 @@ def check_model(module_path, model, expected_runtime):
         return False
     return True
 
-logging.basicConfig(level=logging.INFO)
-
-manifest = tla_utils.load_manifest()
-
 # Ensure longest-running modules go first
+manifest = tla_utils.load_json(manifest_path)
 small_models = sorted(
     [
         (module['path'], model, tla_utils.parse_timespan(model['runtime']))
