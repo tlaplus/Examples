7d7dab921faab749b30872f8fd4f694d6c4bd1aa
Andrew Helwer <2n8rn1w1f@mozmail.com>
2023-06-04 13:16:35 -0400

    Switch to using Community Modules jar for CI
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- .github/scripts/parse_modules.py
+++ .github/scripts/parse_modules.py
@@ -13,13 +13,13 @@ import tla_utils
 parser = ArgumentParser(description='Parses all TLA+ modules in the tlaplus/examples repo using SANY.')
 parser.add_argument('--tools_jar_path', help='Path to the tla2tools.jar file', required=True)
 parser.add_argument('--tlapm_lib_path', help='Path to the TLA+ proof manager module directory; .tla files should be in this directory', required=True)
-parser.add_argument('--community_modules_path', help='Path to the TLA+ community module directory; .tla files should be in this directory', required=True)
+parser.add_argument('--community_modules_jar_path', help='Path to the CommunityModules-deps.jar file', required=True)
 parser.add_argument('--manifest_path', help='Path to the tlaplus/examples manifest.json file', required=True)
 args = parser.parse_args()
 
 tools_jar_path = normpath(args.tools_jar_path)
 tlaps_modules = normpath(args.tlapm_lib_path)
-community_modules = normpath(args.community_modules_path)
+community_modules = normpath(args.community_modules_jar_path)
 manifest_path = normpath(args.manifest_path)
 logging.basicConfig(level=logging.INFO)
 
@@ -28,7 +28,7 @@ def parse_module(path):
     Parse the given module using SANY.
     """
     logging.info(path)
-    search_paths = pathsep.join([tools_jar_path, dirname(path), tlaps_modules, community_modules])
+    search_paths = pathsep.join([tools_jar_path, community_modules, dirname(path), tlaps_modules])
     sany = subprocess.run([
         'java',
         '-cp', search_paths,
